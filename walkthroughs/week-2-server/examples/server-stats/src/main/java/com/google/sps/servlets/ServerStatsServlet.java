135729dbcab8410898008c0cf2d1ba52e4a2a383
Yucheng Wang <monicawang0614@gmail.com>
2022-06-10 02:50:36 +0000

    The next step follows

--- walkthroughs/week-2-server/examples/server-stats/src/main/java/com/google/sps/servlets/ServerStatsServlet.java
+++ walkthroughs/week-2-server/examples/server-stats/src/main/java/com/google/sps/servlets/ServerStatsServlet.java
@@ -73,4 +73,21 @@ public final class ServerStatsServlet extends HttpServlet {
     String json = gson.toJson(serverStats);
     return json;
   }
+
+@WebServlet("/form-handler")
+public class FormHandlerServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Get the value entered in the form.
+    String textValue = request.getParameter("text-input");
+
+    // Print the value so you can see it in the server logs.
+    System.out.println("You submitted: " + textValue);
+
+    // Write the value to the response so the user can see it.
+    response.getWriter().println("You submitted: " + textValue);
+    }
+}
 }
